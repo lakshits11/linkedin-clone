a209b7115dcf0060cfe5f3f283f82f922dd1988f
lakshits11 <lakshits11@gmail.com>
2022-01-16 15:10:54 +0530

    Removed unnecessary import App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Login from "./components/Login";
